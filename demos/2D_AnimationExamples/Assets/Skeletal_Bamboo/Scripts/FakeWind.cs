57795ab402b23f538c8b86b246bc5ccb266d41c2
Owen Mundy <omundy@gmail.com>
2020-01-09 15:30:20 -0500

    Add animation example

---
+++ demos/2D_AnimationExamples/Assets/Skeletal_Bamboo/Scripts/FakeWind.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeWind : MonoBehaviour
+{
+
+    // public axis = 1;
+
+    // void Tilt(){
+    //     Vector3 euler = transform.localEulerAngles;
+    //     euler.z = Mathf.Lerp(euler.z, axis, 2.0f * Time.deltaTime);
+    //     transform.localEulerAngles = euler;
+    // }
+
+
+
+
+    public enum OccilationFuntion { Sine, Cosine }
+    public void Start()
+    {
+        //to start at zero
+        StartCoroutine(Oscillate(OccilationFuntion.Sine, 1f));
+        //to start at scalar value
+        //StartCoroutine (Oscillate (OccilationFuntion.Cosine, 1f));
+    }
+
+    private IEnumerator Oscillate(OccilationFuntion method, float scalar)
+    {
+        while (true)
+        {
+            if (method == OccilationFuntion.Sine)
+            {
+                transform.position = new Vector3(Mathf.Sin(Time.time) * scalar, 0, 0);
+            }
+            else if (method == OccilationFuntion.Cosine)
+            {
+                transform.position = new Vector3(Mathf.Cos(Time.time) * scalar, 0, 0);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+
+
+
+
+}
