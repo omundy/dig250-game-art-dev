cf0159f2f7a47b9e80be3b2db117dab7de7c99c7
Owen Mundy <omundy@gmail.com>
2020-04-13 14:33:15 -0400

    Adds data handline examples

---
+++ demos/Examples_Davidson/Assets/Data_Handling/Scripts/ArrayExample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrayExample : MonoBehaviour
+{
+    // create a new array of strings. length is inferred
+    public string[] colors = new string[] { "red", "green", "blue" };
+
+    void Start()
+    {
+        for (int i = 0; i < colors.Length; i++)
+        {
+            Debug.Log("Array " + i + " = " + colors[i]);
+        }
+    }
+}
---
+++ demos/Examples_Davidson/Assets/Data_Handling/Scripts/StructExample.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct NPC
+{
+    // fields
+    public int NPCId;
+    public string Name;
+    public bool GoodGuy;
+    // constructor
+    public NPC(int _NPCId, string _Name, bool _GoodGuy)
+    {
+        NPCId = _NPCId;
+        Name = _Name;
+        GoodGuy = _GoodGuy;
+    }
+}
+
+public class StructExample : MonoBehaviour
+{
+    // dictionary of strings
+    public Dictionary<string, string> stringDict = new Dictionary<string, string>();
+    // dictionary of structs
+    public Dictionary<string, NPC> structDict = new Dictionary<string, NPC>();
+
+
+
+    void Start()
+    {
+        // add string and print
+        stringDict.Add("greeting", "hello world!");
+        print("StructExample -> stringDict['greeting'] = " + stringDict["greeting"]);
+
+        // add struct and print
+        structDict.Add("badguy1", new NPC(1, "Dr. Evil", false));
+        print("StructExample -> structDict['badguy1'].Name = " + structDict["badguy1"].Name);
+
+    }
+
+
+}
